c4905583a6dcb070d49654e0cdb8b83c1ffb5473
Mwavu <mwavukennedy@gmail.com>
2023-12-24 15:55:46 +0300

    ch: refactor code

--- Val.R
+++ Val.R
@@ -1,48 +1,100 @@
-library(pacman)
-p_load(tidyverse, magrittr, ggthemes, showtext, emojifont)
+library(ggplot2)
+library(ggthemes)
+library(emojifont)
 
-showtext_auto()
-windows(10, 10)
+showtext::showtext_auto()
 
-# loading google fonts:
-font_add_google('Schoolbell', 'bell')
+# load google fonts:
+sysfonts::font_add_google("Schoolbell", "bell")
 
-# to search for emojis available in {emojifont}:
-search_emoji('star')
-search_emoji('rose')
+# search for emojis:
+emojifont::search_emoji("star")
+emojifont::search_emoji("rose")
 
-df <- tibble(
-  t = seq(from = -10, to = 10, by = 0.01), 
-  x = 16 * sin(t) ** 3, 
+df <- tibble::tibble(
+  t = seq(from = -10, to = 10, by = 0.01),
+  x = 16 * sin(t)**3,
   y = 13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t)
 )
 
-ggplot() + 
-  geom_point(data = df %>% filter(x <= 0), mapping = aes(x = x, y = y), colour = "white") + 
-  geom_point(data = df %>% filter(x > 0), mapping = aes(x = x, y = y)) + 
-  geom_line(data = df %>% filter(x > 0), 
-            mapping = aes(x = x, y = y), 
-            color = 'red', alpha = 0.8) + 
-  geom_line(data = df %>% filter(x <= 0), mapping = aes(x = x, y = y)) + 
-  geom_point(mapping = aes(x = c(-10, 10, 0, -5, 5, -15, 15), y = c(-10, -10, 10, rep(-15, 4))), 
-             shape = 23, colour = 'red', fill = 'red', size = 3) + 
-  geom_point(mapping = aes(x = 0, y = -5:-10), 
-             shape = 23, color = 'firebrick', fill = 'firebrick', size = 1.7) + 
-  geom_point(mapping = aes(x = 0, y = -4:5), 
-             shape = 23, color = "black", fill = 'black', size = 2, alpha = 0.7) + 
-  geom_emoji('rose', color = 'white', size = 80, x = -0.2, y = -8, alpha = 0.8) + 
-  geom_emoji('star', x = c(-10, 10, 0, -5, 5, -15, 15), y = c(-10, -10, 10, rep(-15, 4)), size = 10, color = 'red') + 
-  geom_text(mapping = aes(x = 0, y = 0), 
-            label = "Rstats", colour = "white", size = 11, family = 'bell') + 
-  geom_text(mapping = aes(x = 0.2, y = 2), 
-            label = 'Happy Valentines', color = 'white', size = 11, family = 'bell') + 
-  theme_dark() + 
-  coord_fixed() + 
+ggplot() +
+  geom_point(
+    data = df |> dplyr::filter(x <= 0), 
+    mapping = aes(x = x, y = y), 
+    colour = "white"
+  ) +
+  geom_point(
+    data = df |> dplyr::filter(x > 0), 
+    mapping = aes(x = x, y = y)
+  ) +
+  geom_line(
+    data = df |> dplyr::filter(x > 0),
+    mapping = aes(x = x, y = y),
+    color = "red", alpha = 0.8
+  ) +
+  geom_line(
+    data = df |> dplyr::filter(x <= 0), 
+    mapping = aes(x = x, y = y)
+  ) +
+  geom_point(
+    mapping = aes(
+      x = c(-10, 10, 0, -5, 5, -15, 15), 
+      y = c(-10, -10, 10, rep(-15, 4))
+    ),
+    shape = 23, 
+    colour = "red", 
+    fill = "red", 
+    size = 3
+  ) +
+  geom_point(
+    mapping = aes(x = 0, y = -5:-10),
+    shape = 23, 
+    color = "firebrick", 
+    fill = "firebrick", 
+    size = 1.7
+  ) +
+  geom_point(
+    mapping = aes(x = 0, y = -4:5),
+    shape = 23, 
+    color = "black", 
+    fill = "black", 
+    size = 2, 
+    alpha = 0.7
+  ) +
+  emojifont::geom_emoji(
+    alias = "rose", 
+    color = "white", 
+    size = 80, 
+    x = -0.2, 
+    y = -8, 
+    alpha = 0.8
+  ) +
+  emojifont::geom_emoji(
+    alias = "star", 
+    x = c(-10, 10, 0, -5, 5, -15, 15), 
+    y = c(-10, -10, 10, rep(-15, 4)), 
+    size = 10, 
+    color = "red"
+  ) +
+  geom_text(
+    mapping = aes(x = 0, y = 0),
+    label = "Rstats", 
+    colour = "white", 
+    size = 11, 
+    family = "bell"
+  ) +
+  geom_text(
+    mapping = aes(x = 0.2, y = 2),
+    label = "Happy Valentines", 
+    color = "white", 
+    size = 11, 
+    family = "bell"
+  ) +
+  theme_few() +
+  coord_fixed() +
   theme(
     panel.grid = element_blank(), # remove grid lines, axis titles, texts and ticks.
-    axis.title = element_blank(), 
-    axis.text = element_blank(), 
+    axis.title = element_blank(),
+    axis.text = element_blank(),
     axis.ticks = element_blank()
   )
-
-
